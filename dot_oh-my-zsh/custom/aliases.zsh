ecf4100375c822c7b86b42e98d40ddbb1f516c51
Arthur Charpiat <arthur.charpiat@gmail.com>
2026-01-06 16:41:57 +0100

    add: intellij alias & start drawing

--- dot_oh-my-zsh/custom/aliases.zsh
+++ dot_oh-my-zsh/custom/aliases.zsh
@@ -4,3 +4,4 @@ alias bat='batcat'
 alias glg='git lg'
 alias glint='golangci-lint run --allow-parallel-runners --show-stats'
 alias l='eza -1 -F -l -a --git'
+alias idea="/mnt/c/Program\ Files/JetBrains/IntelliJ\ IDEA*/bin/idea64.exe"
